91f9527e905924efe239a79973c667d4544d339c
Kacper Szczęsny <78164353+wonderooo@users.noreply.github.com>
2024-11-24 00:31:48 +0100

    Worker responses come as result, added worker error messages, added copy clone impls for messages (#2)

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,2 +1,76 @@
+use prost::{Message, Oneof};
+use std::ops::{Deref, DerefMut};
+
 pub mod master;
 pub mod worker;
+
+// This is just a wrapper type for `ProtoResult`
+// as enums can not be passed as `Message` with `prost` crate.
+// This type should not be constructed directly
+// but rather with `ProtoResult::into()`.
+// TODO: find a workaround to skip wrapping enums with struct `Message`
+#[derive(Message, Copy, Clone, PartialEq, Eq)]
+pub struct ProtoResultWrapper<T, E>
+where
+    T: Message + Default + Clone + PartialEq + Eq,
+    E: Message + Default + Clone + PartialEq + Eq,
+{
+    // Must be wrapped with an `Option` as `prost` crate requires so.
+    // TODO: find a workaround to skip wrapping enums with an `Option`
+    #[prost(oneof = "ProtoResult", tags = "1, 2")]
+    result: Option<ProtoResult<T, E>>,
+}
+
+#[derive(Oneof, Copy, Clone, PartialEq, Eq)]
+pub enum ProtoResult<T, E>
+where
+    T: Message + Default + Clone + PartialEq + Eq,
+    E: Message + Default + Clone + PartialEq + Eq,
+{
+    #[prost(message, tag = "1")]
+    Ok(T),
+    #[prost(message, tag = "2")]
+    Err(E),
+}
+
+impl<T, E> Into<ProtoResultWrapper<T, E>> for ProtoResult<T, E>
+where
+    T: Message + Default + Clone + PartialEq + Eq,
+    E: Message + Default + Clone + PartialEq + Eq,
+{
+    fn into(self) -> ProtoResultWrapper<T, E> {
+        ProtoResultWrapper { result: Some(self) }
+    }
+}
+
+impl<T, E> Into<Option<ProtoResult<T, E>>> for ProtoResultWrapper<T, E>
+where
+    T: Message + Default + Clone + PartialEq + Eq,
+    E: Message + Default + Clone + PartialEq + Eq,
+{
+    fn into(self) -> Option<ProtoResult<T, E>> {
+        self.result
+    }
+}
+
+impl<T, E> Deref for ProtoResultWrapper<T, E>
+where
+    T: Message + Default + Clone + PartialEq + Eq,
+    E: Message + Default + Clone + PartialEq + Eq,
+{
+    type Target = Option<ProtoResult<T, E>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.result
+    }
+}
+
+impl<T, E> DerefMut for ProtoResultWrapper<T, E>
+where
+    T: Message + Default + Clone + PartialEq + Eq,
+    E: Message + Default + Clone + PartialEq + Eq,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.result
+    }
+}
--- proto/src/worker.rs
+++ proto/src/worker.rs
@@ -1,34 +1,92 @@
-use prost::{Message, Oneof};
+use crate::{ProtoResult, ProtoResultWrapper};
+use prost::{Enumeration, Message, Oneof};
+use std::fmt::Debug;
 
-#[derive(Message)]
+#[derive(Message, Clone, Eq, PartialEq)]
 pub struct Packet {
-    #[prost(oneof = "WorkerMessage", tags = "1")]
+    // Must be wrapped with an `Option` as `prost` crate requires so.
+    #[prost(oneof = "WorkerMessage", tags = "1, 2")]
     pub msg: Option<WorkerMessage>,
 }
 
-#[derive(Oneof)]
+#[derive(Oneof, Clone, PartialEq, Eq)]
 pub enum WorkerMessage {
     #[prost(message, tag = "1")]
-    GenerateResponse(Response),
+    GenerateResponse(ProtoResultWrapper<GenerateResponse, WorkerErrorWrapper>),
+
+    #[prost(message, tag = "2")]
+    LoadResponse(ProtoResultWrapper<LoadResponse, WorkerErrorWrapper>),
 }
 
-#[derive(Message)]
-pub struct Response {
-    #[prost(int32, tag = "1")]
-    pub id: i32,
+#[derive(Message, Clone, PartialEq, Eq)]
+pub struct GenerateResponse {
+    #[prost(uint32, tag = "1")]
+    pub id: u32,
 
     #[prost(string, tag = "2")]
     pub content: String,
+}
+
+#[derive(Message, Copy, Clone, PartialEq, Eq)]
+pub struct LoadResponse {
+    #[prost(uint32, tag = "1")]
+    pub id: u32,
+}
+
+// This is just a wrapper type for `WorkerError`
+// as enums can not be passed as `Message` with `prost` crate.
+// This type should not be constructed directly
+// but rather with `WorkerError::into()`.
+#[derive(Message, Copy, Clone, PartialEq, Eq)]
+pub struct WorkerErrorWrapper {
+    #[prost(enumeration = "WorkerError", tag = "1")]
+    pub r#err: i32,
+}
+
+#[derive(Enumeration, Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(i32)]
+pub enum WorkerError {
+    ModelBusy = 0,
+    ModelAlreadyLoaded = 1,
+    ModelNotLoaded = 2,
+    LoadingError = 3,
+    GenerationError = 4,
 }
 
 impl Packet {
     pub fn new(msg: WorkerMessage) -> Self {
         Self { msg: Some(msg) }
     }
+
+    pub fn new_load_response(response: ProtoResult<LoadResponse, WorkerErrorWrapper>) -> Self {
+        Self {
+            msg: Some(WorkerMessage::LoadResponse(response.into())),
+        }
+    }
+
+    pub fn new_generate_response(
+        response: ProtoResult<GenerateResponse, WorkerErrorWrapper>,
+    ) -> Self {
+        Self {
+            msg: Some(WorkerMessage::GenerateResponse(response.into())),
+        }
+    }
 }
 
-impl Response {
-    pub fn new(id: i32, content: String) -> Self {
+impl GenerateResponse {
+    pub fn new(id: u32, content: String) -> Self {
         Self { id, content }
+    }
+}
+
+impl LoadResponse {
+    pub fn new(id: u32) -> Self {
+        Self { id }
+    }
+}
+
+impl Into<WorkerErrorWrapper> for WorkerError {
+    fn into(self) -> WorkerErrorWrapper {
+        WorkerErrorWrapper { r#err: self as i32 }
     }
 }
