07d8a9431a5ea30996adc96717fafed7b0013e8f
Bartłomiej "itedya" Domański <bdomanski@protonmail.com>
2025-03-16 21:37:54 +0100

    convert proto package to native protobuf

---
+++ proto/build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .file_descriptor_set_path("./compiled/service_descriptor.bin")
+        .out_dir("./compiled")
+        .compile_protos(&["geirmund.proto"], &[""])
+        .unwrap()
+}
