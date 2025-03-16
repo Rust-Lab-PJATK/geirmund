72cddeee2f7ea67432f6b122a9a43edebde29900
Bartłomiej "itedya" Domański <bdomanski@protonmail.com>
2025-03-16 22:20:02 +0100

    fix method of including service descriptor for protobuf

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,3 +1,3 @@
-pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("service_descriptor");
+pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("../compiled/service_descriptor.bin");
 
 include!("../compiled/com.github.rustlabpjatk.geirmund.rs");
