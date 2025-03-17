ab29ca62aab02031a2acc525b19c495545c107d3
Bartłomiej "itedya" Domański <bdomanski@protonmail.com>
2025-03-17 09:52:09 +0100

    Add basic communication between socket function and rest of the code.

--- master/src/main.rs
+++ master/src/main.rs
@@ -1,16 +1,17 @@
-use futures::Stream;
+use futures::{Stream, StreamExt};
 use proto::{MasterPacket, WorkerPacket};
 use std::{net::ToSocketAddrs, pin::Pin};
-use tokio::sync::mpsc;
+use tokio::sync::{broadcast, mpsc};
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_util::sync::CancellationToken;
-use tonic::{transport::Server, Request, Response, Status, Streaming};
+use tonic::{transport::Server, Code, Request, Response, Status, Streaming};
 use tracing::Level;
 
 pub struct MasterServer {
     cancellation_token: CancellationToken,
-    send_request_receiver: mpsc::Receiver<MasterPacket>,
-    receive_response_sender: mpsc::Sender<WorkerPacket>,
+    send_response_tx: broadcast::Sender<MasterPacket>,
+    send_response_rx: broadcast::Receiver<MasterPacket>,
+    receive_request_tx: broadcast::Sender<(core::net::SocketAddr, WorkerPacket)>,
 }
 
 #[tonic::async_trait]
@@ -23,14 +24,67 @@ impl proto::master_server::Master for MasterServer {
         &self,
         request: Request<Streaming<WorkerPacket>>,
     ) -> Result<Response<Self::StreamStream>, Status> {
-        let in_stream = request.into_inner();
+        let socket_addr = request.remote_addr().ok_or_else(|| {
+            tracing::error!("failed to acquire worker remote address, disconnecting the worker");
+            return Status::new(Code::Internal, "failed to acquire worker remote address");
+        })?;
 
-        let (tx, rx) = mpsc::channel(128);
+        tracing::info!("new worker connection from address: {socket_addr}");
 
-        let cancellation_token = self.cancellation_token.clone();
-        tokio::spawn(async move { while !cancellation_token.is_cancelled() {} });
+        // Receive request from worker
 
-        let out_stream = ReceiverStream::new(rx);
+        let mut in_stream = request.into_inner();
+
+        let receive_request_tx = self.receive_request_tx.clone();
+        let receive_request_socket_addr = socket_addr.clone();
+        let receive_request_cancellation_token = self.cancellation_token.clone();
+        tokio::spawn(async move {
+            loop {
+                let event = tokio::select! {
+                    Some(received_event) = in_stream.next() => match received_event {
+                        Ok(event) => event,
+                        Err(e) => {
+                            tracing::error!("GRPC Error received from worker on address {receive_request_socket_addr:?}: {e}");
+                            continue;
+                        }
+                    },
+                    _ = receive_request_cancellation_token.cancelled() => break,
+                };
+
+                receive_request_tx
+                    .send((receive_request_socket_addr, event))
+                    .unwrap();
+            }
+        });
+
+        // Send response to worker
+
+        let mut send_response_tx = self.send_response_tx.subscribe();
+
+        let (out_stream_tx, out_stream_rx) = mpsc::channel(128);
+
+        let send_response_socket_addr = socket_addr;
+        let send_response_cancellation_token = self.cancellation_token.clone();
+        tokio::spawn(async move {
+            loop {
+                let event = tokio::select! {
+                    received_event = send_response_tx.recv() => match received_event {
+                        Ok(event) => event,
+                        Err(e) => {
+                            tracing::error!("recoverable error received on local_send_request_receiver on worker address {send_response_socket_addr:?}: {e}");
+                            continue;
+                        }
+                    },
+                    _ = send_response_cancellation_token.cancelled() => break,
+                };
+
+                if let Err(e) = out_stream_tx.send(Ok(event)).await {
+                    tracing::error!("recoverable error received when trying to pass MasterPacket from local_send_request_receiver to tx: {e}");
+                }
+            }
+        });
+
+        let out_stream = ReceiverStream::new(out_stream_rx);
 
         Ok(Response::new(Box::pin(out_stream) as Self::StreamStream))
     }
@@ -57,6 +111,8 @@ async fn main() {
 
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
+    tracing::info!("Creating reflection service...");
+
     let reflection_service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
         .build_v1()
@@ -64,14 +120,17 @@ async fn main() {
 
     let cancellation_token = CancellationToken::new();
 
-    let (send_request_sender, send_request_receiver) = mpsc::channel(128);
-    let (receive_response_sender, receive_response_receiver) = mpsc::channel(128);
+    let (send_response_tx, send_response_rx) = broadcast::channel(128);
+    let (receive_request_tx, receive_request_rx) = broadcast::channel(128);
 
     let server = MasterServer {
         cancellation_token: cancellation_token.clone(),
-        send_request_receiver,
-        receive_response_sender,
+        send_response_tx,
+        send_response_rx,
+        receive_request_tx,
     };
+
+    tracing::info!("Starting GRPC tcp listener... (there will be no confirmation log)");
 
     Server::builder()
         .add_service(reflection_service)
