baf6a145efb797d4de957d6ed9b0a259971858f3
Bartłomiej "itedya" Domański <bdomanski@protonmail.com>
2025-03-17 08:41:38 +0100

    Handle another edge case in proto/build.rs that throws error when fresh compiling
    For some unknown reason (to me), the proto/build.rs throws error on fresh git clone.
    I fixed it with simple std::fs::exists checks.

--- proto/build.rs
+++ proto/build.rs
@@ -3,11 +3,19 @@ fn main() {
     println!("cargo:rerun-if-changed=compiled");
     println!("cargo:rerun-if-changed=src/main.rs");
 
+    if !std::fs::exists("./compiled").unwrap() {
+        std::fs::create_dir("./compiled").unwrap();
+    }
+
+    if !std::fs::exists("./compiled/service_descriptor.bin").unwrap() {
+        std::fs::write("./compiled/service_descriptor.bin", "").unwrap();
+    }
+
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
-        .file_descriptor_set_path("./compiled/service_descriptor.bin")
         .out_dir("./compiled")
+        .file_descriptor_set_path("./compiled/service_descriptor.bin")
         .compile_protos(&["protobufs/geirmund.proto"], &["protobufs"])
         .unwrap()
 }
